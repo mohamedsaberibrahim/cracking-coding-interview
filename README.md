382470c5c17cdbf5a6f048cc84395ea15518d3f4
Mohamed Saber <mohamed.saber@bosta.co>
2023-08-04 10:12:39 +0300

    feat: add cli file generator on js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# cracking-coding-interview
+# Cracking Coding Interview Solutions
+
+## To use the CLI file generator:
+- Run `node file-generator -n <filename>`
+- Files will be generated on JS at the base directory
---
+++ file-generator/index.js
@@ -0,0 +1,41 @@
+const { Command } = require('commander');
+const fs = require('fs');
+const handlebars = require('handlebars');
+
+const program = new Command();
+
+program
+  .name('generate')
+  .description('Generate a file and its test case')
+  .requiredOption('-n, --filename <filename>, File Name')
+  .option('-t, --type <type>', 'File type (e.g. js, ts)')
+  .option('-d, --description <description>', 'File description')
+
+program.parse(process.argv);
+const options = program.opts();
+
+// Define template variables based on options
+const vars = {
+  filename: options.filename || '',
+  description: options.description || '',
+  // Add any other necessary variables here
+};
+
+// Read file and test case templates
+const fileTemplate = fs.readFileSync(`${__dirname}/templates/fileTemplate.hbs`, 'utf8');
+const testTemplate = fs.readFileSync(`${__dirname}/templates/testCaseTemplate.hbs`, 'utf8');
+
+// Compile templates using Handlebars
+const compiledFileTemplate = handlebars.compile(fileTemplate);
+const compiledTestTemplate = handlebars.compile(testTemplate);
+
+// Generate file and test case content
+const fileContent = compiledFileTemplate(vars);
+const testContent = compiledTestTemplate(vars);
+
+// Write file and test case to disk
+fs.writeFileSync(`${vars.filename}.${options.type || 'js'}`, fileContent);
+fs.writeFileSync(`${vars.filename}.test.${options.type || 'js'}`, testContent);
+
+console.log(`File and test case generated: ${vars.filename}.${options.type || 'js'}`);
+
